9ada2d8b53a30ee20e9996e7628e83b164dc7bea
T-107 <T-107@PC263>
2015-07-17 21:41:11 -0500

    controlador

--- src/main/java/org/unitec/maven/ControladorLeer.java
+++ src/main/java/org/unitec/maven/ControladorLeer.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller//Estereotipo: nestructura es un controler.
 @RequestMapping("/")//http://localhost:9000/
 public class ControladorLeer {
-    @RequestMapping(value = "/leer",method = RequestMethod.GET,headers = {"Accept=text/html"})
+    @RequestMapping(value = "/leer",method = RequestMethod.GET,
+            headers = {"Accept=text/html"})//headers tipo de dato que le quiero dar al navegador.
     @ResponseBody String leer()throws Exception{
         return "aqui vamos a mostrar el servicio leer que hicimos ya";
     }
