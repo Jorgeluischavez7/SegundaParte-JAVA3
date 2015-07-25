575f85049090a9616d467881ff94be276cc1dae3
T-107 <T-107@PC263>
2015-07-24 21:42:49 -0500

    Proyecto

---
+++ src/main/java/org/unitec/maven/DAOProducto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.maven;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author T-107
+ */
+
+public class DAOProducto {
+    //tenemos que crear algo parecido al parcial.
+    public static void guardar(Producto p)throws Exception{
+        SessionFactory fac= HibernateUtilidades.getSessionFactory();
+        Session ses= fac.openSession();
+        Transaction tranza= ses.begin.Transaction();
+        //guardamos.
+        ses.save(p);
+        ses.close();
+    }
+    
+}
