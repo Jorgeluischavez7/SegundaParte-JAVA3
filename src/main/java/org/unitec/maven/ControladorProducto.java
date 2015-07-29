3a63f76ab3258011da19ee25ef0d06b8d8ac5e81
T-107 <T-107@PC263>
2015-07-28 21:43:15 -0500

    Metodo actualizar en el controlador del producto

--- src/main/java/org/unitec/maven/ControladorProducto.java
+++ src/main/java/org/unitec/maven/ControladorProducto.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.unitec.maven;
 
 import org.springframework.stereotype.Controller;
@@ -20,13 +16,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ControladorProducto {
     @RequestMapping(value="/producto/{nombre}/{costo}/{unidades}",method=RequestMethod.POST, headers ={"Accept=text/html"})
     @ResponseBody String guardar(@PathVariable String nombre, @PathVariable Float costo,@PathVariable Integer unidades)throws Exception{
-    Producto p=new Producto();
+    DAOProducto da=new DAOProducto();
+        Producto p=new Producto();
     p.setCosto(costo);
     p.setNombre(nombre);
     p.setUnidades(unidades);
-        DAOProducto.guardar(p);
-        return "Tu producto se ha guardado con exito";
-        
+        da.guardar(p);
+        return "Tu producto se ha guardado con exito!!";       
     }
-    
+    @RequestMapping(value="/producto/{id_producto}/{nombre}/{costo}/{unidades}",method=RequestMethod.PUT, headers ={"Accept=text/html"})
+    @ResponseBody String actualizar(@PathVariable Integer id_producto,@PathVariable String nombre, @PathVariable Float costo,@PathVariable Integer unidades)throws Exception{
+        DAOProducto dao=new DAOProducto();
+        Producto p=new Producto();
+        p.setIdProducto(id_producto);
+        p.setNombre(nombre);
+        p.setCosto(costo);
+        p.setUnidades(unidades);
+        dao.actualizar(p);
+        return "Producto actulaizado con exito";
+    }
 }
--- src/main/java/org/unitec/maven/DAOProducto.java
+++ src/main/java/org/unitec/maven/DAOProducto.java
@@ -15,14 +15,41 @@ import org.hibernate.Transaction;
  */
 
 public class DAOProducto {
+    private SessionFactory fac;
+    private Session ses;
+    private Transaction tranza;
+    
+    public DAOProducto(){//cosntructor que inicializa los atributos que son vistos por todos los metodos.
+        fac= HibernateUtilidades.getSessionFactory();
+        ses= fac.openSession();
+        tranza= ses.beginTransaction();
+    }
+    
+    public void cerrarSesion(){
+        tranza.commit();
+        ses.close();
+                
+    }
+    
+    
+    public void guardar(Producto g)throws Exception{
+    ses.save(g);
+    cerrarSesion();
+    }
+   
+   
+    
     //tenemos que crear algo parecido al parcial.
-    public static void guardar(Producto p)throws Exception{
+   /* public static void guardar(Producto p)throws Exception{
         SessionFactory fac= HibernateUtilidades.getSessionFactory();
         Session ses= fac.openSession();
         Transaction tranza= ses.beginTransaction();
         //guardamos.
         ses.save(p);
         ses.close();
+    }*/
+    public  void actualizar(Producto g)throws Exception{
+        ses.update(g);
+        cerrarSesion();
     }
-    
 }
